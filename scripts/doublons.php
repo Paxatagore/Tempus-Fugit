86b14e62d53bcd18c15ca91f856db13ac091c84a
Paxatagore <Matthieu.duclos@gmail.com>
2015-08-15 17:49:44 +0200

    Correction du script doublons.php
    Aparement, la requête de suppression à la fin ne marcherait pas ?

--- scripts/doublons.php
+++ scripts/doublons.php
@@ -7,7 +7,83 @@ $lienep	= new lienep() ;
 $lienpl	= new lienpl() ; 
 $rpp	= new relationpersonnes() ;
 $p->select() ;
+
+$q = "SELECT COUNT( nom ) AS nbr_doublon, nom FROM personne GROUP BY nom HAVING COUNT( nom ) >1" ;
+mysqldb::send($q) ;
+$personnes = array() ;
+while ($row = mysqldb::$req->fetch()) {
+	$personnes[] = $row["nom"] ;
+}
+mysqldb::$req->closeCursor() ;
+
+foreach ($personnes as $pnom) {
+	//étape 1 - on identifie les différents numéros
+	echo "J'ai identifié le doublon $pnom. <br/>" ;
+	$q2 = 'SELECT num, autreNom, dynastie, url FROM personne WHERE nom = "'.$pnom.'"' ;
+	mysqldb::send($q2) ;
+	$row = mysqldb::$req->fetch() ;
+	$num1 = $row[0] ;
+	$autreNom = $row[1] ;
+	$dynastie = $row[2] ;
+	$url	= $row[3] ;
+	$num2 = array() ;
+	$autreNoms2 = array() ;
+	while ($row = mysqldb::$req->fetch()) {
+		$num2[] = $row[0] ;
+		if ($row[1] != "") $autreNoms2[] = $row[1] ;
+		if ($dynastie == 0 && $row[2] !=0) $dynastie = $row[2] ;	
+		if ($url == 0 && $row[3] !=0) $url = $row[3] ;			
+	}
+	mysqldb::$req->closeCursor() ;
+	$num2 = join($num2, ",") ;
+	echo "Le numéro originel est ".$num1.". Par la suite, il y a ".count($num2)." doublon(s) : ".$num2.". <br/>" ;
+	
+	//1ère étape de la fusion : les liens EL sont tous transférés sur le premier individu
+	$q3 = "UPDATE lienep SET personne = $num1 WHERE personne IN ($num2)" ;
+	echo "Requête de changement des liens EP : $q3<br/>" ;
+	//mysqldb::send($q3) ;
+	
+	//2e étape de la fusion : les liens PL
+	$q4 = "UPDATE lienpl SET personne = $num1 WHERE personne IN ($num2)" ;
+	echo "Requête de changement des liens PL : $q4<br/>" ;
+	//mysqldb::send($q4) ;		
+		
+	//3e étape de la fusion : les liens PP
+	$q5 = "UPDATE relationpersonnes SET personne1 = $num1 WHERE personne1 IN ($num2)" ;
+	echo "Requête de changement des liens PL : $q4<br/>" ;
+	//mysqldb::send($q5) ;	
+	$q6 = "UPDATE relationpersonnes SET personne2 = $num1 WHERE personne2 IN ($num2)" ;
+	echo "Requête de changement des liens PL : $q4<br/>" ;
+	//mysqldb::send($q6) ;	
+	
+	//4e étape - fusion des autres noms
+	if (count($autreNoms2) > 0) {
+		$autreNom .= ",".joint(autreNoms2, ",") ;
+	}
+	
+	//5e étape - fusion des dynasties
+	//déjà fait plus haut
+	//6e étape - fusion des url
+	//déjà fait plus haut
+
+	$valide = 0 ;
+	//on sauve la personne 1
+	$q7 = 'UPDATE personne SET autreNom = "'.$autreNom.'", dynastie = "'.$dynastie.'", url = "'.$url.'", valide = 0 WHERE num = '.$num1 ;
+	echo "Requête d'enregistrement de la personne : $q7<br/>" ;
+	//mysqldb::send($q7) ;
+	//on supprime les autres
+	$q8 = "DELETE FROM personne WHERE num IN ($num2)" ;
+	echo "Requête de suppression des autres personne : $q8<br/>" ;
+	//mysqldb::send($q8) ;
+}
+
+/*
+$personnes = array() ;
 while($p->next()) {
+	$personnes[] = $p->num ;
+}
+
+foreach($personnes as $pnum) {
 	$p2->select('WHERE num > '.$p->num.' AND nom = "'.$p->nom.'"') ;
 	while($p2->next()) {
 		echo "$p->num et $p2->num sont un doublons ($p->nom). <br/>" ;
@@ -64,7 +140,7 @@ while($p->next()) {
 		$p->save() ;
 		$p2->delete() ;
 	}
-}
+}*/
 
 echo "<br/>Le script a bien été exécuté.<br/>" ;
 ?>
