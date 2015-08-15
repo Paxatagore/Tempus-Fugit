4525da150a4e14730c9d658a03fa75878160005c
Paxatagore <Matthieu.duclos@gmail.com>
2015-08-15 17:05:43 +0200

    Adaptation du script qui tuait les personnes vides.
    Vérifier qu'il marche bien.

--- scripts/tuePersonnesVides.php
+++ scripts/tuePersonnesVides.php
@@ -3,31 +3,19 @@
 require_once("../inc/centrale.php") ;
 $p = new personne() ;
 $p->select('WHERE nom=""') ;
+$isin = array() ;
 while($p->next()) {
 	echo $p->num." n'a pas de nom : ".$p->nom.". Je le tue. <br/>" ;
 	$p->delete() ;
+	$isin[] = $p->num ;
 }
 
-//vérifie ensuite les liens PP
-$l = new relationpersonnes() ;
-$p = new personne() ;
-$l->select() ;
-while ($l->next()) {
-	$p->num = 0 ;
-	$p->get($l->personne1) ;
-	if ($p->num == 0) {
-		echo "$l->personne1 n'existe plus. Je supprime le lien PP $l->num. <br/>" ;
-		$l->delete() ;
-	}
-	else {
-		$p->num = 0 ;
-		$p->get($l->personne2) ;
-		if ($p->num == 0) {
-			echo "$l->personne2 n'existe plus. Je supprime le lien PP $l->num. <br/>" ;
-			$l->delete() ;
-		}
-	}
-}
+$isin = join($isin, ",") ;
+
+$q = "DELETE FROM relationpersonnes WHERE personne1 IN ($isin)" ;
+mysqldb::send($q) ;
+$q = "DELETE FROM relationpersonnes WHERE personne2 IN ($isin)" ;
+mysqldb::send($q) ;
 
 echo "<br/>Le script a bien été exécuté.<br/>" ;
 ?>
